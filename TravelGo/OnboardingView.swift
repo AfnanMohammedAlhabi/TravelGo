e3e02af23d5a15cd2bc3d0755ce275bb9e7b3beb
Hind <hindalzarah@outlook.com>
2022-11-01 10:36:09 +0300

    onboarding done

---
+++ TravelGo/OnboardingView.swift
@@ -0,0 +1,76 @@
+//
+//  OnBoard 1.swift
+//  TravelGo
+//
+//  Created by Afnan on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+struct OnBoard_1: View {
+    @State private var pageIndex = 0
+    private let pages: [Page] = Page.samplePages
+    private let dotAppearance = UIPageControl.appearance()
+    var body: some View {
+        TabView(selection: $pageIndex) {
+            ForEach(pages) { page in
+                VStack {
+                    HStack{
+                        HStack{
+                            if pageIndex == 0 || pageIndex == 1 {
+                                Spacer()
+                                Button("Skip") {
+                                    goToLast()
+                                    
+                                }
+                            }
+                            
+                        }.padding()
+                    }
+                    
+                    Spacer()
+                    PageView(page: page)
+                    Spacer()
+                    if page == pages.last {
+                        Button("Sign up!", action: goToZero)
+                            .buttonStyle(.bordered)
+                    } else {
+                        Button("next", action: incrementPage)
+                            .buttonStyle(.borderedProminent)
+                    }
+                    Spacer()
+                }
+                .tag(page.tag)
+            }
+        }
+        // for animations (sliding)
+        .animation(.easeInOut, value: pageIndex)// 2
+        .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+        // for the dots appearance
+        .tabViewStyle(PageTabViewStyle())
+        .onAppear {
+            dotAppearance.currentPageIndicatorTintColor = .black
+            dotAppearance.pageIndicatorTintColor = .gray
+            
+        }
+    }
+    func incrementPage() {
+        pageIndex += 1
+    }
+    
+    func goToZero() {
+        pageIndex = 0
+    }
+    func goToLast()
+    {
+        pageIndex = 2
+        
+    }
+    
+}
+
+struct OnBoard_1_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoard_1()
+    }
+}
